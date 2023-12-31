dc7064bcc40af4fead66a73213fab50a63b8ca16
Joker13 <120789407+jokerko13@users.noreply.github.com>
2023-12-31 13:09:05 +0100

    Add files via upload

---
+++ drz_utility/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'DRZ TEAM'
+description 'DRZ_UTILITY By DRZ TEAM'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    'shared/*.lua'
+}
+
+client_script {
+    'client/*.lua'
+}
+
+escrow_ignore {
+    'shared/*.lua',
+}
+
+dependencies {
+	'es_extended'
+}
+
+dependency '/assetpacks'
